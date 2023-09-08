91902bab2f9e286d826ee633ae9b8f1770bd1634
vinayjain2408 <vinay98292jain@gmail.com>
2023-09-08 10:32:38 +0530

    complete Activity gauge

--- src/Solid.js
+++ src/Solid.js
@@ -3,174 +3,43 @@ import Highcharts from "highcharts/highcharts.js";
 import HighchartsReact from "highcharts-react-official";
 import highchartsMore from "highcharts/highcharts-more";
 import solidGauge from "highcharts/modules/solid-gauge";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowUp, faCircle, faSquare } from "@fortawesome/free-solid-svg-icons"; // Import FontAwesome icons
-
 
 
 highchartsMore(Highcharts);
 solidGauge(Highcharts);
 
-// function renderIcons(chart) {
-//   // Move icon
-//   if (!chart.series[0].icon) {
-//     chart.series[0].icon = chart.renderer
-//       .path(["M", -8, 0, "L", 8, 0, "M", 0, -8, "L", 8, 0, 0, 8])
-//       .attr({
-//         stroke: "#303030",
-//         "stroke-linecap": "round",
-//         "stroke-linejoin": "round",
-//         "stroke-width": 2,
-//         zIndex: 10,
-//       })
-//       .add(chart.series[2].group);
-//   }
-//   chart.series[0].icon.translate(
-//     chart.chartWidth / 2 - 10,
-//     chart.plotHeight / 2 -
-//       chart.series[0].points[0].shapeArgs.innerR -
-//       (chart.series[0].points[0].shapeArgs.r -
-//         chart.series[0].points[0].shapeArgs.innerR) /
-//         2
-//   );
-
-//   // Exercise icon
-//   if (!chart.series[1].icon) {
-//     chart.series[1].icon = chart.renderer
-//       .path([
-//         "M",
-//         -8,
-//         0,
-//         "L",
-//         8,
-//         0,
-//         "M",
-//         0,
-//         -8,
-//         "L",
-//         8,
-//         0,
-//         0,
-//         8,
-//         "M",
-//         8,
-//         -8,
-//         "L",
-//         16,
-//         0,
-//         8,
-//         8,
-//       ])
-//       .attr({
-//         stroke: "#ffffff",
-//         "stroke-linecap": "round",
-//         "stroke-linejoin": "round",
-//         "stroke-width": 2,
-//         zIndex: 10,
-//       })
-//       .add(chart.series[2].group);
-//   }
-//   chart.series[1].icon.translate(
-//     chart.chartWidth / 2 - 10,
-//     chart.plotHeight / 2 -
-//       chart.series[1].points[0].shapeArgs.innerR -
-//       (chart.series[1].points[0].shapeArgs.r -
-//         chart.series[1].points[0].shapeArgs.innerR) /
-//         2
-//   );
-
-//   // Stand icon
-//   if (!chart.series[2].icon) {
-//     chart.series[2].icon = chart.renderer
-//       .path(["M", 0, 8, "L", 0, -8, "M", -8, 0, "L", 0, -8, 8, 0])
-//       .attr({
-//         stroke: "#303030",
-//         "stroke-linecap": "round",
-//         "stroke-linejoin": "round",
-//         "stroke-width": 2,
-//         zIndex: 10,
-//       })
-//       .add(chart.series[2].group);
-//   }
-
-//   chart.series[2].icon.translate(
-//     chart.chartWidth / 2 - 10,
-//     chart.plotHeight / 2 -
-//       chart.series[2].points[0].shapeArgs.innerR -
-//       (chart.series[2].points[0].shapeArgs.r -
-//         chart.series[2].points[0].shapeArgs.innerR) /
-//         2
-//   );
-// }
 function renderIcons() {
-
   // Move icon
   if (!this.series[0].icon) {
-      this.series[0].icon = this.renderer.path(['M', -7, 0, 'L', 7, 0, 'M', 0, -7, 'L', 7, 0, 0, 7])
-          .attr({
-              stroke: '#303030',
-              'stroke-linecap': 'round',
-              'stroke-linejoin': 'round',
-              'stroke-width': 2,
-              zIndex: 10
-          })
-          .add(this.series[0].group);
+    this.series[0].icon = this.renderer
+      .path(["M", -7, 0, "L", 7, 0, "M", 0, -7, "L", 7, 0, 0, 7])
+      .attr({
+        stroke: "#303030",
+        "stroke-linecap": "round",
+        "stroke-linejoin": "round",
+        "stroke-width": 3,
+        zIndex: 10,
+      })
+      .add(this.series[0].group);
   }
   this.series[0].icon.translate(
-      this.chartWidth / 2 - 10,
-      this.plotHeight / 2 - this.series[0].points[0].shapeArgs.innerR -
-          (this.series[0].points[0].shapeArgs.r - this.series[0].points[0].shapeArgs.innerR) / 2
+    this.chartWidth / 2 - 10,
+    this.plotHeight / 2 -
+      this.series[0].points[0].shapeArgs.innerR -
+      (this.series[0].points[0].shapeArgs.r -
+        this.series[0].points[0].shapeArgs.innerR) /
+        2
   );
 
-  // Exercise icon
-  if (!this.series[1].icon) {
-      this.series[1].icon = this.renderer.path(
-          ['M', -7, 0, 'L', 7, 0, 'M', 0, -7, 'L', 7, 0, 0, 7,
-              'M', 7, -7, 'L', 14, 0, 7, 7]
-      )
-          .attr({
-              stroke: '#ffffff',
-              'stroke-linecap': 'round',
-              'stroke-linejoin': 'round',
-              'stroke-width': 2,
-              zIndex: 10
-          })
-          .add(this.series[2].group);
-  }
-  this.series[1].icon.translate(
-      this.chartWidth / 2 - 10,
-      this.plotHeight / 2 - this.series[1].points[0].shapeArgs.innerR -
-          (this.series[1].points[0].shapeArgs.r - this.series[1].points[0].shapeArgs.innerR) / 2
-  );
-
-  // Stand icon
-  if (!this.series[2].icon) {
-      this.series[2].icon = this.renderer.path(['M', 0, 7, 'L', 0, -7, 'M', -7, 0, 'L', 0, -7, 7, 0])
-          .attr({
-              stroke: '#303030',
-              'stroke-linecap': 'round',
-              'stroke-linejoin': 'round',
-              'stroke-width': 2,
-              zIndex: 10
-          })
-          .add(this.series[2].group);
-  }
-
-  this.series[2].icon.translate(
-      this.chartWidth / 2 - 10,
-      this.plotHeight / 2 - this.series[2].points[0].shapeArgs.innerR -
-          (this.series[2].points[0].shapeArgs.r - this.series[2].points[0].shapeArgs.innerR) / 2
-  );
 }
 
 const GaugeChart = {
-  // ... Your other chart options ...
   chart: {
     type: "solidgauge",
     height: "30%",
-    events :{
-      render:renderIcons
-    }
+    events: {
+      render: renderIcons,
+    },
   },
 
   title: {
@@ -198,57 +67,22 @@ const GaugeChart = {
     },
   },
 
-
   pane: {
     startAngle: 0,
     endAngle: 360,
     background: [
       {
         // Track for Move
-        outerRadius: "90%",
-        innerRadius: "90%",
+        outerRadius: "112%",
+        innerRadius: "88%",
+        backgroundColor: Highcharts.color(Highcharts.getOptions().colors[0])
+          .setOpacity(0.3)
+          .get(),
         borderWidth: 0,
-        borderColor: "transparent",
       },
-      {
-        outerRadius: "88%",
-        innerRadius: "65%",
-        borderWidth: 0,
-        borderColor: "transparent",
-        events: {
-          render: function () {
-            renderIcons(this.chart); // Call the renderIcons function
-          },
-        },
-      },
-      {
-        outerRadius: "63%",
-        innerRadius: "40%",
-        borderWidth: 0,
-        borderColor: "transparent",
-        events: {
-          render: function () {
-            renderIcons(this.chart); // Call the renderIcons function
-          },
-        },
-      },
+    
     ],
   },
-  
-
-
-  //             )
-  //             .css({
-  //               fontSize: "24px", // Adjust the font size as needed
-  //               color: "red", // Adjust the color as needed
-  //             })
-  //             .add();
-  //         },
-  //       },
-  //     },
-  //   ],
-  // },
-
 
   yAxis: {
     min: 0,
@@ -260,61 +94,32 @@ const GaugeChart = {
   plotOptions: {
     solidgauge: {
       dataLabels: {
-        enabled: true,
+        enabled: false,
       },
       linecap: "round",
       stickyTracking: false,
-      rounded: false,
+      rounded: true,
     },
   },
 
-  series: [
-    {
-      name: "Move",
-      type: "solidgauge",
-      data: [
-        {
-          color: "#e6cb00",
-          radius: "115%",
-          innerRadius: "90%",
-          y: 80,
-        },
-      ],
-    },
-    {
-        name: "Move",
-        type: "solidgauge",
-        data: [
-          {
-            color: "red",
-            icon:<faArrowUp />,
-            radius: "88%",
-            innerRadius: "65%",
-            y: 75,
-          },
-        ],
-      },
-      {
-        name: "Move",
-        type: "solidgauge",
-        data: [
-          {
-            color: "blue",
-            radius: "63%",
-            innerRadius: "40%",
-            y: 70,
-          },
-        ],
-      },
-  ],
-  // ... Rest of your chart options ...
+
+
+  series: [{
+    name: 'Move',
+    data: [{
+        color: Highcharts.getOptions().colors[0],
+        radius: '112%',
+        innerRadius: '88%',
+        y: 30
+    }]
+}]
+
 };
 
 const Solid = () => {
   const refContainer = useRef(null);
 
   useEffect(() => {
-    // Update the chart when the component mounts
     refContainer.current.chart.reflow();
   }, []);
 
